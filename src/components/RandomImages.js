f92a46073ae9cb3678a3fa5439eb2854d3ca0c63
Meera <colorcodedcrayons@gmail.com>
2023-06-02 19:00:20 +0530

    get random images in Home page

--- src/components/RandomImages.js
+++ src/components/RandomImages.js
@@ -1,5 +1,37 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
 function RandomImages() {
-    return (<div> RandomImages </div>)
+  const [randomImages, setRandomImages] = useState([]);
+
+  const fetchImages = async () => {
+    
+    const response = await axios.get(
+        `https://api.unsplash.com/photos?client_id=${process.env.REACT_APP_UNSPLASH_ACCESS_KEY}`
+      )
+      .then((data) => {
+        console.log("data", data.data[0].urls.small)
+        setRandomImages(data.data)
+      })
+      .catch((error) => console.log(error));    
+    // console.log("response", response)
+  }
+
+  useEffect(() => {
+    // Fetch 10 random images from the server
+    // fetch("backend-server/random-images")
+    fetchImages()
+    // console.log("randomImages", randomImages)
+  }, []);
+
+  return (
+    <div>
+      <h1>Random Images</h1>
+      {randomImages.map((image, index) => (
+        <img key={index} src={image.urls.small} alt={`Random Image ${index}`} />
+      ))}
+    </div>
+  );
 }
 
 export default RandomImages;
